94e738c0567e7096fb0b20a846c78eb1b6307b39
Pavlo Kytsmey <pkyt@macmini42.local>
2014-01-28 18:16:20 +0200

    adding missing files

---
+++ Calculator_iOS2Tests/Calculator_iOS2Tests.m
@@ -0,0 +1,34 @@
+//
+//  Calculator_iOS2Tests.m
+//  Calculator_iOS2Tests
+//
+//  Created by Pavlo Kytsmey on 1/27/14.
+//  Copyright (c) 2014 Pavlo Kytsmey. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Calculator_iOS2Tests : XCTestCase
+
+@end
+
+@implementation Calculator_iOS2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
